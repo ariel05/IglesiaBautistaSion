c4f0dc43fa4e3837b43c3783eb1a11660cd684fb
Ariel Rivero <ariel200506@gmail.com>
2019-07-17 01:05:21 -0300

    Agrego proyecto

---
+++ src/main/java/com/iglesiaBaustistaSion/spring/IglesiaBautistaSionApplication.java
@@ -0,0 +1,13 @@
+package com.iglesiaBaustistaSion.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IglesiaBautistaSionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IglesiaBautistaSionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iglesiaBaustistaSion/spring/IglesiaBautistaSionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iglesiaBaustistaSion.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IglesiaBautistaSionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
